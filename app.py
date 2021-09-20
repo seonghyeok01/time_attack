fbe5ee3de189c0ee0f4417ab1b196bbdbbafd139
ssh <32880ask@naver.com>
2021-09-20 12:28:58 +0900

    데이터 수정

--- app.py
+++ app.py
@@ -1,41 +1,81 @@
 from flask import Flask, render_template, jsonify, request
+from pymongo import MongoClient
+import requests
+from bs4 import BeautifulSoup
+
 app = Flask(__name__)
 
 
-
-from pymongo import MongoClient
 client = MongoClient('localhost', 27017)
-db = client.dbsparta
+db = client.dbStock
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/base/codes', methods=['GET'])
+def get_base_codes():
+    codes = list(db.codes.find({}).distinct("group"))
+    return jsonify(codes)
+
+
+@app.route('/codes', methods=['GET'])
+def get_codes():
+    group = request.args.get('group')
+    codes = list(db.codes.find({'group': group}, {'_id': False}))
+    return jsonify(codes)
+
+
+@app.route('/stocks', methods=['POST'])
+def save_info():
+    info = request.json
+    stocks = list(db.stocks.find(info, {'_id': False}))
+    db.searchs.insert_one(info)
+    return jsonify(stocks)
 
 
 
+@app.route('/stock/like', methods=['PUT'])
+def set_like():
+    info = request.json
+    db.stocks.update_one({"code": info['code']}, {"$set": {"isLike": True}})
+    return "success"
 
-codes = [
-    {"group": "market", "code": "market-1", "name": "코스피"},
-    {"group": "market", "code": "market-2", "name": "코스닥"},
-    {"group": "sector", "code": "sector-1", "name": "반도체와반도체장비"},
-    {"group": "sector", "code": "sector-2", "name": "양방향미디어와서비스"},
-    {"group": "sector", "code": "sector-3", "name": "자동차"},
-    {"group": "tag", "code": "tag-1", "name": "메모리"},
-    {"group": "tag", "code": "tag-2", "name": "플랫폼"},
-    {"group": "tag", "code": "tag-4", "name": "전기차"},
-]
-db.codes.insert_many(codes)
 
-stocks = [
-        {"name": "삼성전자", "code": "005930", "sector":"sector-1", "market": "market-1", "tag": "tag-1"},
-        {"name": "SK하이닉스", "code": "000660", "sector":"sector-1", "market": "market-1", "tag": "tag-1"},
-        {"name": "리노공업", "code": "058470", "sector": "sector-1", "market": "market-2", "tag": "tag-1"},
-        {"name": "DB하이텍", "code": "000990", "sector": "sector-1", "market": "market-1", "tag": "tag-1"},
-        {"name": "솔브레인", "code": "357780", "sector": "sector-1", "market": "market-2", "tag": "tag-1"},
-        {"name": "카카오", "code": "357780", "sector": "sector-2", "market": "market-1", "tag": "tag-2"},
-        {"name": "네이버", "code": "035420", "sector": "sector-2", "market": "market-1", "tag": "tag-2"},
-        {"name": "아프리카TV", "code": "067160", "sector": "sector-2", "market": "market-2", "tag": "tag-3"},
-        {"name": "자이언트스텝", "code": "289220", "sector": "sector-2", "market": "market-2", "tag": "tag-2"},
-        {"name": "키다리스튜디오", "code": "020120", "sector": "sector-2", "market": "market-1", "tag": "tag-2"},
-        {"name": "현대차", "code": "005380", "sector": "sector-3", "market": "market-1", "tag": "tag-4"},
- ]
-db.stocks.insert_many(stocks)
+@app.route('/stock/unlike', methods=['PUT'])
+def set_unlike():
+    info = request.json
+    db.stocks.update_one({"code": info['code']}, {"$set": {"isLike": False}})
+    return "success"
 
-if __name__ == '__main__':
-   app.run('0.0.0.0',port=5000,debug=True)
+@app.route('/stocks/like', methods=['GET'])
+def get_stocks():
+    stocks = list(db.stocks.find({"isLike": True}, {'_id': False}))
+    return jsonify(stocks)
+
+
+@app.route('/stock', methods=['GET'])
+def get_info():
+    code = request.args.get('code')
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+
+    data = requests.get('https://finance.naver.com/item/main.nhn?code=' + code, headers=headers)
+    soup = BeautifulSoup(data.text, 'html.parser')
+    amount = soup.select_one('#_market_sum').text
+    amount = amount.replace('\n', '')
+    amount = amount.replace('\t', '')
+    if soup.select_one('#_per') is None:
+        per = 'N/A'
+    else:
+        per = soup.select_one('#_per').text
+    price = soup.select_one(
+        '#content > div.section.trade_compare > table > tbody > tr:nth-child(1) > td:nth-child(2)').text
+
+    return jsonify({'amount': amount, 'per': per, 'price': price})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
